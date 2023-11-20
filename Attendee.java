7b36d6b5404ebd773b44e54005ff4d97a860ca10
averaeth26 <114428954+averaeth26@users.noreply.github.com>
2023-11-20 13:15:51 -0500

    Added more data to attendants and created more helper functions (getters/setters).

--- Attendee.java
+++ Attendee.java
@@ -1,9 +1,40 @@
-public class Attendee {
-String name;
-String company;
+import java.util.ArrayList;
 
-public Attendee (String attendeeName, String attendeeCompany) {
-	name = currentName;
-	company = attendeeCompany;
+public class Attendee {
+	String name;
+	int companyNum;
+	String company;
+	int id;
+	int tableNum;
+	int seatNum;
+
+	public Attendee(String attendeeName, int attendeeCompany, int attendeeId) {
+		name = attendeeName;
+		companyNum = attendeeCompany;
+		id = attendeeId;
+		}
+		
+	public int getId() {
+		return id;
 	}
+	
+	public int getTableNum() {
+		return tableNum;
+	}
+	
+	public int getCompanyNum() {
+		return companyNum;
+	}
+
+	public void setTableNum(int num) {
+		tableNum = num;
+	}
+	
+	public void removeAttendee(Attendee attendee, ArrayList<Attendee> attendants) {
+		attendants.remove(attendee.getId()-1);
+	}
+		
+	public String toString() {
+		return "NAME: " + name + "  " + "COMPANY: " + companyNum + "  " + "ID: " + id;
+		}
 }
