f1fb051ef82e58ea45c4450c99d47008237791ca
averaeth26 <114428954+averaeth26@users.noreply.github.com>
2023-11-20 13:14:32 -0500

    Tester Class now creates and tests the party creation functionality.

--- Tester.java
+++ Tester.java
@@ -1,6 +1,10 @@
-
+import java.util.ArrayList;
 public class Tester {
-  public static void main(String[] args) {
-	  
+	public static void main(String[] args) {
+		Party p1 = new Party();
+	    ArrayList<Attendee> attendants = p1.generateAttendance();
+	    for (Attendee attendee: attendants) {
+			System.out.println(attendee);
+		}
   }
 }
