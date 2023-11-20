81ca36d6e469e40f3b67bbd0037808acb367c6fd
averaeth26 <114428954+averaeth26@users.noreply.github.com>
2023-11-20 13:12:07 -0500

    Worked on being able to fill tables with attendees.

--- Table.java
+++ Table.java
@@ -2,5 +2,29 @@ import java.util.ArrayList;
 
 public class Table {
 	int numSeats;
-	ArrayList<Attendee> people = ArrayList<Attendee>();
+	int numTables = 10;
+	ArrayList<Attendee> people = new ArrayList<Attendee>();
+	
+	public boolean checkForCompany(int checkCompanyNum, ArrayList<Attendee> table) {
+		for (Attendee guest: table) {
+			if (guest.getCompanyNum() == checkCompanyNum) {
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	public ArrayList<Attendee> fillTable (ArrayList<Attendee> attendants) {
+		ArrayList<Attendee> table = new ArrayList<Attendee>();  
+		for (Attendee attendee: attendants) {
+			boolean check = checkForCompany(attendee.getCompanyNum(), attendants);
+			if (!check && attendee.getTableNum() <= numTables) {
+				table.add(attendee);
+			}
+			if (table.size() >= numTables) {
+				return table;
+			}
+		}
+		return table;	
+	}
 }
